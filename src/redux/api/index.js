26c965dc09391da947045b16b7b57b3066c67dee
iskewed <jtornello2019@hotmail.com>
2020-06-26 15:44:19 -0300

    Solved a problem with the API. Changed HTTP to HTTPS

--- src/redux/api/index.js
+++ src/redux/api/index.js
@@ -1,10 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
 
-const baseURL = 'http://www.omdbapi.com/?apikey=5c6d9a6c';
+const baseURL = "https://www.omdbapi.com/?apikey=5c6d9a6c";
 
-export const apiCall = (url, data, headers, method) => axios({
+export const apiCall = (url, data, headers, method) =>
+  axios({
     method,
     url: baseURL + url,
     data,
-    headers
-});
+    headers,
+  });
